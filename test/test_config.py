f39338b2f85e5d597d6c8d4c55fecd0148bb0eec
Mike Kerzhner <mikerzhner@gmail.com>
2019-02-04 14:47:53 -0700

    Fresh test address

--- test/test_config.py
+++ test/test_config.py
@@ -1,5 +1,6 @@
 from bot.config import get_bot, K_NAME
 
+
 def test_get_bot():
-    bot2 = get_bot(index=2)
-    assert bot2[K_NAME] == 'Grand Master Bot'
+    bot2 = get_bot(index=0)
+    assert bot2[K_NAME] == 'Young Bot'
